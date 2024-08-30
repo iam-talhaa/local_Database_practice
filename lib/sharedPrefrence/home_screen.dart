0be2f5c31bd3c4c65188cdc50e743316d8c0a1f9
iam-talhaa <tahakhan4141@gmail.com>
2024-08-30 22:27:41 +0500

    updated

--- lib/sharedPrefrence/home_screen.dart
+++ lib/sharedPrefrence/home_screen.dart
@@ -54,6 +54,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   onChanged: (value) {
                     print('');
                     print('');
+                    print('');
+                    print('');
                     setState(() {});
                   }),
             ])
